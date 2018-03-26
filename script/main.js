0af7f72682dc2ea1734fd45abc0502b7ecd7c1c8
dipopa <37536121+dipopa@users.noreply.github.com>
2018-03-26 18:22:30 +0300

    Completed step 1
    Clicking a small image -> it becomes the main image. No css or html were harmed in the completion of this assignment :)

--- script/main.js
+++ script/main.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log('Hello Diana');
 });
+var mainimg = document.getElementsByClassName('main')[0];
+var imglist = document.getElementsByClassName('img-list');
+for (var i=0; i<imglist.length; i++) {
+  var clickedimg = imglist[i];
+  clickedimg.addEventListener('click', function(event) {
+    mainimg.src = event.target.src;
+  });
+}
