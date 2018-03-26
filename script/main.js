a4203bd7d0d7fe8d160ffd60e10e10f43eebb744
dipopa <37536121+dipopa@users.noreply.github.com>
2018-03-26 23:29:20 +0300

    Completed step 3

--- script/main.js
+++ script/main.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log('Hello Diana');
 });
-var mainimg = document.getElementsByClassName('main')[0];
-var imglist = document.getElementsByClassName('img-list');
-for (var i=0; i<imglist.length; i++) {
-  var clickedimg = imglist[i];
-  clickedimg.addEventListener('click', function(event) {
+function imgCycle() {
+  var mainimg = document.getElementsByClassName('main')[0];
+  var imglist = document.getElementsByTagName('img');
+  for (var i=0; i<imglist.length; i++) {
     var currentmain = mainimg.src;
-    mainimg.src = event.target.src;
-    event.target.src = currentmain;
-  });
+    mainimg.src = imglist[i].src;
+    imglist[i].src = currentmain;
+  }
+  setTimeout(imgCycle, 3000);
 }
+imgCycle();
