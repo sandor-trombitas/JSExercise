f90c33eeb1c067e7996fe12b61ae5b8037bfd8b9
dipopa <37536121+dipopa@users.noreply.github.com>
2018-04-03 07:56:30 +0300

    Finished exercise 4

--- script/main.js
+++ script/main.js
@@ -4,11 +4,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
 function imgCycle() {
   var mainimg = document.getElementsByClassName('main')[0];
   var imglist = document.getElementsByTagName('img');
-  for (var i=0; i<imglist.length; i++) {
+for (var i=0; i<imglist.length; i++) {
     var currentmain = mainimg.src;
     mainimg.src = imglist[i].src;
     imglist[i].src = currentmain;
   }
   setTimeout(imgCycle, 3000);
 }
+var descriptionIndex = 0;
+function showDescription() {
+  var i;
+  var descriptionList = document.getElementsByClassName('description');
+  for (i=0; i<descriptionList.length; i++) {
+    descriptionList[i].style.display = 'none';
+  }
+  descriptionIndex++;
+  if (descriptionIndex > descriptionList.length) {descriptionIndex=1}
+  descriptionList[descriptionIndex-1].style.display = 'block';
+  setTimeout(showDescription, 3000);
+}
 imgCycle();
+showDescription();
